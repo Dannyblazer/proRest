545c704d5f8c4c929c1db1d3dce456f1a75d8430
Dannyblazer <nnajidanny004@gmail.com>
2023-06-23 23:26:12 +0100

    Refactored views

--- catalog/views.py
+++ catalog/views.py
@@ -1,21 +1,9 @@
-from django.shortcuts import render
-from django.http import HttpResponse
-from rest_framework.renderers import JSONRenderer
-from rest_framework.parsers import JSONParser
+from rest_framework.response import Response
+from rest_framework import generics
 from .models import Product
 from .serializers import ProductSerializer
 # Create your views here.
 
-class JSONResponse(HttpResponse):
-    """ Basically renders this content into json"""
-    def __init__(self, data, **kwargs):
-        content = JSONRenderer().render(data)
-        kwargs['content_type'] = 'application/json'
-        super(JSONResponse, self).__init__(content, **kwargs)
-
-
-def product_list(request):
-    if request.method == 'GET':
-        products = Product.objects.all()
-        serializer = ProductSerializer(products, many=True)
-        return JSONResponse(serializer.data)
+class ProductList(generics.ListAPIView):
+    queryset = Product.objects.all()
+    serializer_class = ProductSerializer
