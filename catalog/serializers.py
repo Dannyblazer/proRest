75ad8da808619697b8d3d1a253db015abf3f05b2
Dannyblazer <nnajidanny004@gmail.com>
2023-06-24 01:01:25 +0100

    Added product ID to fields.

--- catalog/serializers.py
+++ catalog/serializers.py
@@ -4,6 +4,6 @@ from rest_framework import serializers
 class ProductSerializer(serializers.ModelSerializer):
     class Meta:
         model = Product
-        fields = ('name', 'description', 'price')
+        fields = ('id', 'name', 'description', 'price')
 
     
