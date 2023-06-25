4c24f383ecf4f1dc4be45f3747769a64fdd78b06
Dannyblazer <nnajidanny004@gmail.com>
2023-06-25 23:40:46 +0100

    added the reviews model.

--- catalog/models.py
+++ catalog/models.py
@@ -1,5 +1,6 @@
 from __future__ import unicode_literals
 from django.db import models
+from django.contrib.auth.models import User
 # Create your models here.
 
 class Product(models.Model):
@@ -7,4 +8,11 @@ class Product(models.Model):
      description = models.TextField()
      price = models.DecimalField(decimal_places=2, max_digits=20)
 
+
+class Review(models.Model):
+     product = models.ForeignKey(Product, related_name='reviews', on_delete=models.CASCADE)
+     title = models.CharField(max_length=25)
+     review = models.TextField()
+     rating = models.IntegerField()
+     created_by = models.ForeignKey(User, on_delete=models.CASCADE)
 
