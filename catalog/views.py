5660e9c6e1916521048f6d2911ecfbd275594f43
Dannyblazer <nnajidanny004@gmail.com>
2023-06-25 23:38:11 +0100

    Intergrated the reviews views.

--- catalog/views.py
+++ catalog/views.py
@@ -1,11 +1,11 @@
 from rest_framework import status
 from django.http import Http404
-from .permissions import IsAdminOrReadOnly
+from .permissions import IsAdminOrReadOnly, IsOwnerOrReadOnly
 from rest_framework.response import Response
 from rest_framework import generics
-from rest_framework.views import APIView
-from .models import Product
-from .serializers import ProductSerializer
+from rest_framework.permissions import IsAuthenticatedOrReadOnly
+from .models import Product, Review
+from .serializers import ProductSerializer, ReviewSerializer
 # Create your views here.
 
 class ProductList(generics.ListCreateAPIView):
@@ -14,27 +14,28 @@ class ProductList(generics.ListCreateAPIView):
     permission_classes = (IsAdminOrReadOnly, )
 
 
-class ProductDetail(APIView):
-    def get_object(self, pk):
-        try:
-            return Product.objects.get(pk=pk)
-        except Product.DoesNotExist:
-            raise Http404
-    
-    def get(self, request, pk, format=None):
-        product = self.get_object(pk)
-        serializer = ProductSerializer(product)
-        return Response(serializer.data)
+class ProductDetail(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Product.objects.all()
+    serializer_class = ProductSerializer
+    permission_classes = (IsAdminOrReadOnly, )
 
-    def put(self, request, pk, format=None):
-        product = self.get_object(pk)
-        serializer = ProductSerializer(product, data=request.data)
-        if serializer.is_valid():
-            serializer.save()
-            return Response(serializer.data)
-        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+class ReviewList(generics.ListCreateAPIView):
+    queryset = Review.objects.all()
+    serializer_class = ReviewSerializer
+    permission_classes = (IsAuthenticatedOrReadOnly, )
+
+    def perform_create(self, serializer):
+        serializer.save(
+            created_by = self.request.user,
+            product_id = self.kwargs['pk']
+        )
+
+class ReviewDetail(generics.RetrieveUpdateDestroyAPIView):
+    serializer_class = ReviewSerializer
+    permission_classes = (IsAdminOrReadOnly, IsOwnerOrReadOnly, )
+    lookup_url_kwarg = 'review_id'
+
+    def get_queryset(self):
+        review = self.kwargs['review_id']
+        return Review.objects.filter(id=review)
     
-    def delete(self, request, pk, format=None):
-        product = Product.objects.get(pk)
-        product.delete()
-        return Response(status=status.HTTP_204_NO_CONTENT)
